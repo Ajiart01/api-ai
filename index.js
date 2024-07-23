bd3c227661bb319d59f6a9f81b1798d8aef90d87
Ajiart01 <139549928+Ajiart01@users.noreply.github.com>
2024-07-23 15:17:43 +0700

    Update index.js

--- index.js
+++ index.js
@@ -20,6 +20,9 @@ inject();
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
+app.get('/sitemap.xml', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'sitemap.xml'));
+});
 app.get('/ask', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'gpt4.html'));
 });
